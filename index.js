e4599db7ed101de6a7679bc01ce3929d1306ae91
Esma-Uddin <uddinesma@gmail.com>
2024-08-02 19:37:03 +0400

    creating index

---
+++ index.js
@@ -0,0 +1,15 @@
+//const name="Esma";
+//const lastname="Uddin";
+//const age=19;
+//const city="Baku";
+//const profession="developer"
+///const bio=`My name is ${name} ${lastname}.I'am ${age}.I live in ${city} as a ${profession}.`;
+//console.log(bio.toUpperCase())
+//let profession="fullstack_developer"
+
+//console.log(profession.replace("_"))
+let profession="fullstack_developer"
+
+result=profession.replace("_","").length
+result=profession.split("")
+console.log(result)
